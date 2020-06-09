80c0d244f9e226e3e47c726566ff33590bffcce6
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-06-09 11:08:39 +0530

    initial commit
    adding basic files

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  var description = new Typed("#description", {
+    strings: [
+      "Tech Enthusiast^2500",
+      "Python Programmer^2500",
+      "Flutter Developer^2500",
+      "Public Speaker^2500",
+      "Open Source Contributor^2500",
+      "YouTube Creator^2500",
+      "Petrolhead^2500",
+      "Music Lover^2500",
+    ],
+    shuffle: true,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    cursorChar: "_",
+  });
+});
